da82af780f49a76388d57d9dfeaa0953027f7e89
Riya Yadav <riyastdnew.com@gmail.com>
2026-01-16 22:54:42 +0530

    Update API baseURL to production backend

--- quiz-app/src/api/courseApi.js
+++ quiz-app/src/api/courseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://quiz-n2fp1ibzp-riya-yadavs-projects-29ff28d4.vercel.app",
   headers: {
     "x-api-key": "12345",
   },
